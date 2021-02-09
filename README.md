168b681c7d1ba7889b63a268be0df9e96325ed45
Emirhan Kemal Kösem <emirhankemalkosem@gmail.com>
2021-02-09 23:48:57 +0300

    chore: project init 🎉

---
+++ README.md
@@ -0,0 +1,5 @@
+# Remotion Playground
+
+This project will give some code samples for creating cool videos using remotion library.
+
+Further reading [Remotion](https://www.remotion.dev/docs/)
---
+++ src/Video.tsx
@@ -0,0 +1,48 @@
+import {Composition} from 'remotion';
+import {HelloWorld} from './HelloWorld';
+import {Logo} from './HelloWorld/Logo';
+import {Subtitle} from './HelloWorld/Subtitle';
+import Teaser from './Teaser';
+
+export const RemotionVideo: React.FC = () => {
+	return (
+		<>
+			<Composition
+				id="HelloWorld"
+				component={HelloWorld}
+				durationInFrames={150}
+				fps={30}
+				width={1920}
+				height={1080}
+				defaultProps={{
+					titleText: 'Welcome to Remotion',
+					titleColor: 'black',
+				}}
+			/>
+			<Composition
+				id="Logo"
+				component={Logo}
+				durationInFrames={200}
+				fps={30}
+				width={1920}
+				height={1080}
+			/>
+			<Composition
+				id="Title"
+				component={Subtitle}
+				durationInFrames={100}
+				fps={30}
+				width={1920}
+				height={1080}
+			/>
+			<Composition
+				id="Teaser"
+				component={Teaser}
+				durationInFrames={20}
+				fps={30}
+				width={1920}
+				height={1080}
+			/>
+		</>
+	);
+};
